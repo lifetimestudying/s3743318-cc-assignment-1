24f0a9c16d44237f095972f814f66e7672891ee8
Adam Wang <wangxinchenadam@gmail.com>
2021-03-25 01:27:03 +1100

    Update forms.py
    - password length requirement added

--- forms.py
+++ forms.py
@@ -12,7 +12,7 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     userID = StringField('Login ID', validators=[DataRequired(), Length(min=9, max=9)])
     username = StringField('Username', validators=[DataRequired()]) 
-    password = PasswordField('Password', validators=[DataRequired()]) 
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6, max=6)]) 
     confirmpassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='password must match')])
     submitField = SubmitField('Register')
 
